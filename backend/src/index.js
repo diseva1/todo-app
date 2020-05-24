8fd3228bffe529916dc799a3323b1744c12b69b0
DISEVA <diseva.dev@gmail.com>
2020-05-24 16:58:56 +0200

    Deleted boostrap front and dockerized backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,14 @@
 const app = require('./app');
 const { connect } = require('./db');
+const port = process.env.PORT;
 
 async function main() {
-    //Database connection
-    await connect();  
-    //Starts Express
-    await app.listen(4000, function() {
-    console.log('Server on port 4000');
-});
-  
+	//Database connection
+	await connect();
+	//Starts Express
+	await app.listen(port, function () {
+		console.log(`Server on port ${port}`);
+	});
 }
 
 main();
-
-
-
-
--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -6,43 +6,43 @@ const verify = require('../verifyToken');
 const Task = require('../models/Task');
 
 router.get('/', verify, async (req, res) => {
-    const todos = await Task.find({owner: req.user._id});
-    var taskQuantity = await Task.countDocuments({owner: req.user._id});
-    res.json({
-        taskNumber: taskQuantity,
-        todos: todos,
-        user: req.user
-    });
+	const todos = await Task.find({ owner: req.user._id });
+	var taskQuantity = await Task.countDocuments({ owner: req.user._id });
+	res.json({
+		taskNumber: taskQuantity,
+		todos: todos,
+		user: req.user,
+	});
 });
 
 router.post('/add', verify, async (req, res) => {
-    var data = req.body;
-    res.json({message: "Post request made", data: data});
-    await Task.create({
-        title: data.title,
-        description: data.description,
-        priority: data.priority,
-        owner: req.user._id
-    });
+	var data = req.body;
+	res.json({ message: 'Post request made', data: data });
+	await Task.create({
+		title: data.title,
+		description: data.description,
+		priority: data.priority,
+		owner: req.user._id,
+	});
 });
 
-router.delete('/delete', verify, async (req,res) => {
-    var data = req.body;
-    var deleted;
-    const checkOwner = await Task.find({owner: req.user._id});
-    for(var i=0; i<checkOwner.length; i++ ){
-        if(checkOwner[i]._id === data.id) {
-            await Task.findByIdAndDelete(data.id, async (err, res) => {
-                if(res){
-                    console.log('Success');
-                    deleted = true;
-                } else {
-                    deleted = false
-                }
-            });
-        }
-    }
-    res.json({message: "Delete request made", deleted: deleted});
+router.delete('/delete', verify, async (req, res) => {
+	var data = req.body;
+	var deleted;
+	const checkOwner = await Task.find({ owner: req.user._id });
+	for (var i = 0; i < checkOwner.length; i++) {
+		if (checkOwner[i]._id === data.id) {
+			await Task.findByIdAndDelete(data.id, async (err, res) => {
+				if (res) {
+					console.log('Success');
+					deleted = true;
+				} else {
+					deleted = false;
+				}
+			});
+		}
+	}
+	res.json({ message: 'Delete request made', deleted: deleted });
 });
 
 module.exports = router;
